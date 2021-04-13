0a307c64f2e5bc4f792015f4d5b965469d2ba723
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-04-13 11:57:29 -0300

    Config inital storyboard

---
+++ MyPlaygroundBook/Template/PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/Controllers/IntroductionController.swift
@@ -0,0 +1,18 @@
+//
+//  IntroductionController.swift
+//  BookCore
+//
+//  Created by Ronaldo Gomes on 13/04/21.
+//
+
+import UIKit
+
+class IntroductionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
