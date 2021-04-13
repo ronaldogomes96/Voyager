1e8211fd752fc5de163e8f590413f45d89b67c03
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-04-13 12:15:37 -0300

    Protocol to help instance of one storyboard

--- MyPlaygroundBook/Template/PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/LiveViewSupport.swift
+++ MyPlaygroundBook/Template/PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/LiveViewSupport.swift
@@ -11,16 +11,26 @@ import PlaygroundSupport
 /// Instantiates a new instance of a live view.
 ///
 /// By default, this loads an instance of `LiveViewController` from `LiveView.storyboard`.
-public func instantiateLiveView() -> PlaygroundLiveViewable {
+public func instantiateLiveView(_ controller: Controller) -> PlaygroundLiveViewable {
     let storyboard = UIStoryboard(name: "LiveView", bundle: nil)
 
     guard let viewController = storyboard.instantiateInitialViewController() else {
         fatalError("LiveView.storyboard does not have an initial scene; please set one or update this function")
     }
-
-    guard let liveViewController = viewController as? IntroductionController else {
-        fatalError("LiveView.storyboard's initial scene is not a LiveViewController; please either update the storyboard or this function")
+    
+    var liveViewController: LiveView
+    
+    switch controller {
+    case .introduction:
+        liveViewController = viewController as! IntroductionController  
     }
 
     return liveViewController
+}
+
+
+public class LiveView: UIViewController {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
